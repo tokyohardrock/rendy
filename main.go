0c12f6ddfb32bb6748615d007df4f871441ff107
tokyohardrock <tokyohardrock@gmail.com>
2025-11-22 20:26:35 +0200

    Simple light + backface culling

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math"
 	"time"
 )
 
 const (
-	d     = 2.5 // camera Z-coordinate
-	gridW = 40  // grid width
-	gridH = 40  // grid height
-	fps   = 60
+	d     = 3  // camera Z-coordinate
+	gridW = 40 // grid width
+	gridH = 40 // grid height
+	fps   = 16
 	delta = math.Pi * 0.01
 )
 
-type vertices3D struct {
+var backedLight = false
+
+type points struct {
 	x []float64
 	y []float64
 	z []float64
@@ -25,37 +28,47 @@ type face struct {
 	vn int
 }
 
-var vrtcs vertices3D = vertices3D{
-	x: []float64{-1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0},
-	y: []float64{-1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0},
-	z: []float64{-1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0},
+var vrtcs points = points{
+	x: []float64{1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0},
+	y: []float64{-1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0},
+	z: []float64{-1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0},
+}
+
+var normals points = points{
+	x: []float64{0.0, 0.0, 1.0, -1.0, 0.0, 0.0},
+	y: []float64{-1.0, 1.0, 0.0, 0.0, 0.0, 0.0},
+	z: []float64{0.0, 0.0, 0.0, 0.0, 1.0, -1.0},
 }
 
 var faces []face = []face{
-	{v: []int{3, 7, 8}, vn: 1},
-	{v: []int{3, 8, 4}, vn: 1},
+	{v: []int{1, 2, 3}, vn: 1},
+	{v: []int{1, 3, 4}, vn: 1},
 
-	{v: []int{1, 5, 6}, vn: 2},
-	{v: []int{1, 6, 2}, vn: 2},
+	{v: []int{5, 7, 6}, vn: 2},
+	{v: []int{5, 8, 7}, vn: 2},
 
-	{v: []int{7, 3, 2}, vn: 3},
-	{v: []int{7, 2, 6}, vn: 3},
+	{v: []int{1, 5, 6}, vn: 3},
+	{v: []int{1, 6, 2}, vn: 3},
 
-	{v: []int{4, 8, 5}, vn: 4},
-	{v: []int{4, 5, 1}, vn: 4},
+	{v: []int{4, 7, 8}, vn: 4},
+	{v: []int{4, 3, 7}, vn: 4},
 
-	{v: []int{8, 7, 6}, vn: 5},
-	{v: []int{8, 6, 5}, vn: 5},
+	{v: []int{2, 6, 7}, vn: 5},
+	{v: []int{2, 7, 3}, vn: 5},
 
-	{v: []int{3, 4, 1}, vn: 6},
-	{v: []int{3, 1, 2}, vn: 6},
+	{v: []int{1, 8, 5}, vn: 6},
+	{v: []int{1, 4, 8}, vn: 6},
 }
 
 var xyzRotations = []float64{0, 0, 0}
 
-var ascii []byte = []byte{' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'}
+var light = []float64{1, 1, 0}
 
-func normalizeAndCenter(vrt *vertices3D) {
+var cameraDir = []float64{0, 0, -1}
+
+var ascii = []byte{' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'}
+
+func normalizeAndCenter(vrt *points) {
 	if len(vrt.x) == 0 || len(vrt.y) == 0 || len(vrt.z) == 0 {
 		log.Fatal("Empty vertices — nothing to normalize")
 	}
@@ -104,7 +117,7 @@ func getScreenCoord(x, y, z float64) (float64, float64) {
 	return scrnX, scrnY
 }
 
-func findDepth(coords vertices3D, vrtX, vrtY, faceArea float64) float64 {
+func findDepth(coords points, vrtX, vrtY, faceArea float64) float64 {
 	var s1 = math.Abs((vrtX-coords.x[1])*(coords.y[2]-coords.y[1])-(coords.x[2]-coords.x[1])*(vrtY-coords.y[1])) * 0.5
 	var s2 = math.Abs((vrtX-coords.x[2])*(coords.y[0]-coords.y[2])-(coords.x[0]-coords.x[2])*(vrtY-coords.y[2])) * 0.5
 	var a = s1 / faceArea
@@ -113,16 +126,16 @@ func findDepth(coords vertices3D, vrtX, vrtY, faceArea float64) float64 {
 	return a*(1/coords.z[0]) + b*(1/coords.z[1]) + c*(1/coords.z[2])
 }
 
-func scanlineFilling(coords vertices3D) vertices3D {
+func scanlineFilling(coords points) points {
 	if len(coords.x) < 3 {
 		log.Println("Invalid face - must be at least 3 vertices")
-		return vertices3D{}
+		return points{}
 	}
 	var faceArea = math.Abs((coords.x[1]-coords.x[0])*(coords.y[2]-coords.y[0])-(coords.x[2]-coords.x[0])*(coords.y[1]-coords.y[0])) * 0.5
 	if faceArea == 0 {
-		return vertices3D{}
+		return points{}
 	}
-	var filling vertices3D = vertices3D{
+	var filling points = points{
 		x: make([]float64, 0, 5),
 		y: make([]float64, 0, 5),
 		z: make([]float64, 0, 5),
@@ -140,9 +153,6 @@ func scanlineFilling(coords vertices3D) vertices3D {
 	var midY = int(math.Min(gridH-1, math.Ceil(coords.y[1])))
 	var endY = int(math.Min(gridH-1, math.Ceil(coords.y[2])))
 	for y := startY; y < midY; y++ {
-		if coords.y[2] == coords.y[0] {
-			continue
-		}
 		var lim1 = coords.x[0] + (float64(y)-coords.y[0])*(coords.x[1]-coords.x[0])/(coords.y[1]-coords.y[0])
 		var lim2 = coords.x[0] + (float64(y)-coords.y[0])*(coords.x[2]-coords.x[0])/(coords.y[2]-coords.y[0])
 		var startX = int(math.Max(0, math.Ceil(min(lim1, lim2))))
@@ -154,9 +164,6 @@ func scanlineFilling(coords vertices3D) vertices3D {
 		}
 	}
 	for y := midY; y < endY; y++ {
-		if coords.y[1] == coords.y[0] {
-			continue
-		}
 		var lim1 = coords.x[1] + (float64(y)-coords.y[1])*(coords.x[2]-coords.x[1])/(coords.y[2]-coords.y[1])
 		var lim2 = coords.x[0] + (float64(y)-coords.y[0])*(coords.x[2]-coords.x[0])/(coords.y[2]-coords.y[0])
 		var startX = int(math.Max(0, math.Ceil(min(lim1, lim2))))
@@ -170,13 +177,29 @@ func scanlineFilling(coords vertices3D) vertices3D {
 	return filling
 }
 
+func backfaceCulling(x, y, z float64) bool {
+	return (cameraDir[0]*x + cameraDir[1]*y + cameraDir[2]*z) <= 0
+}
+
+func faceBrightness(x, y, z float64) int {
+	var brightness = x*light[0] + y*light[1] + z*light[2]
+	brightness = ((brightness + 3) / 6) * float64(len(ascii))
+	return int(brightness)
+}
+
 func draw() {
+	var now = time.Now()
 	var grid = make([][]int, gridH)
 	var zBuffer = make([][]float64, gridH)
-	var projCoords vertices3D = vertices3D{
+	var projCoords = points{
 		x: make([]float64, len(vrtcs.x)),
 		y: make([]float64, len(vrtcs.y)),
 		z: make([]float64, len(vrtcs.z)),
+	}
+	var tNormals = points{
+		x: make([]float64, len(normals.x)),
+		y: make([]float64, len(normals.y)),
+		z: make([]float64, len(normals.z)),
 	}
 	for i := range grid {
 		grid[i] = make([]int, gridW)
@@ -192,8 +215,23 @@ func draw() {
 		projCoords.y[i] = scrnY
 		projCoords.z[i] = tZ + d
 	}
+	for i := range normals.x {
+		tNormals.x[i], tNormals.y[i], tNormals.z[i] = getTransformedCoords(normals.x[i], normals.y[i], normals.z[i])
+	}
+	var drawnFaces int = len(faces)
 	for i := range faces {
-		var currFace vertices3D = vertices3D{
+		var nIndex = faces[i].vn - 1
+		if backfaceCulling(tNormals.x[nIndex], tNormals.y[nIndex], tNormals.z[nIndex]) {
+			drawnFaces--
+			continue
+		}
+		var brightness int
+		if backedLight {
+			brightness = faceBrightness(normals.x[nIndex], normals.y[nIndex], normals.z[nIndex])
+		} else {
+			brightness = faceBrightness(tNormals.x[nIndex], tNormals.y[nIndex], tNormals.z[nIndex])
+		}
+		var currFace points = points{
 			x: make([]float64, 0, len(faces[i].v)),
 			y: make([]float64, 0, len(faces[i].v)),
 			z: make([]float64, 0, len(faces[i].v)),
@@ -207,17 +245,18 @@ func draw() {
 		var filling = scanlineFilling(currFace)
 		for j := range filling.x {
 			if zBuffer[int(filling.y[j])][int(filling.x[j])] > filling.z[j] {
-				grid[int(filling.y[j])][int(filling.x[j])] = 1
+				grid[int(filling.y[j])][int(filling.x[j])] = brightness
 				zBuffer[int(filling.y[j])][int(filling.x[j])] = filling.z[j]
 			}
 		}
 	}
 	for i := range grid {
 		for j := range grid[i] {
-			print(" " + string(ascii[grid[i][j]]) + " ")
+			fmt.Print(" " + string(ascii[grid[i][j]]) + " ")
 		}
-		print("\n")
+		fmt.Println()
 	}
+	fmt.Printf("Render time: %0.1f ms\tFaces: %d", float64(time.Since(now))/1e6, drawnFaces)
 }
 
 func main() {
